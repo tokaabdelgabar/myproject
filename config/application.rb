d2d43fd55d4b0e034b94dcbad4d632d509324a8c
Maria <zehuiwangzhw@gmail.com>
2016-07-26 01:03:18 +0200

    corrected file

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Team
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
 
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
