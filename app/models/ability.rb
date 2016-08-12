b2efffa1d62b3d644ef8545f86b88318122890b7
Maria <zehuiwangzhw@gmail.com>
2016-08-12 08:21:45 +0200

    removed ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,12 +1,6 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user)
-    can :read, :all
-
-        can :access, :rails_admin
-        can :dashboard
-        can :manage, :all
 
   end
 end
