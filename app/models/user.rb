7a08b9963d22eccaefcfa65a364de1614e959105
bichtramvt243 <nguyenbichtramvt243@gmail.com>
2016-08-17 22:39:00 +0200

    Removed email confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 	# Include default devise modules. Others available are:
 	# :confirmable, :lockable, :timeoutable and :omniauthable
 	devise :database_authenticatable, :registerable, 
-	:recoverable, :rememberable, :trackable, :validatable, :confirmable
+	:recoverable, :rememberable, :trackable, :validatable
 
 	validates_format_of :email,:with => Devise::email_regexp
  #code for getting and setting the list of roles a user belongs to. 
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -126,7 +126,7 @@ Devise.setup do |config|
   # initial account confirmation) to be applied. Requires additional unconfirmed_email
   # db field (see migrations). Until confirmed, new email is stored in
   # unconfirmed_email column, and copied to email column on successful confirmation.
-  config.reconfirmable = true
+  config.reconfirmable = false
 
   # Defines which key will be used when confirming an account
   # config.confirmation_keys = [:email]
@@ -134,7 +134,7 @@ Devise.setup do |config|
   # ==> Configuration for :rememberable
   # The time the user will be remembered without asking for credentials again.
   # config.remember_for = 2.weeks
-  config.reconfirmable = false
+  #config.reconfirmable = false
   # Invalidates all the remember me tokens when the user signs out.
   config.expire_all_remember_me_on_sign_out = true
 
