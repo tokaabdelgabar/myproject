4587c14539367fabf4e3abc8043a0aa06d3d02f8
Maria <zehuiwangzhw@gmail.com>
2016-08-23 12:49:57 +0200

    fixed english page

--- config/routes.rb
+++ config/routes.rb
@@ -1,53 +1,43 @@
 Rails.application.routes.draw do
-  resources :help_items
-  resources :tabs
-  get 'set_language/english'
 
-  get 'set_language/german'
+  #devise
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users, controllers: { confirmations: 'confirmations' }
 
-  get '/dictionary' => 'pages#dictionary'
-  get '/help' => 'help_items#index'
-  get '/disclaimer' => 'tabs#index'
-  get '/notification' => 'notifications#index'
-  get 'messages/new'
-
-Rails.application.routes.draw do
-  resources :help_items
-  resources :tabs
-  get 'set_language/english'
-
-  get 'set_language/german'
-
+  #language scope
   scope "(:locale)", locale: /en|de/ do
     root to: "apps#index" 
     get "apps/index"
-  end  
-end
+    match 'store/:id' => 'store#show', :as => :store_product, :via => :get
+  end
 
-resources :apps do
-  resources :reviews
-end
-
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users, controllers: { confirmations: 'confirmations' }
+  #resources
+  resources :help_items
+  resources :tabs
   resources :categories
-
-# Contact
+  resources :apps do
+    resources :reviews
+  end
+  #Contact
   resources :messages, only: [:new, :create]
 
   resources :apps do
     collection do
       get 'search'
     end
-    resources :reviews, except: [:show, :index]
-    #collection { post :search, to: 'apps#index' }
-end
-  scope "(:locale)", locale: /en|de/ do
-  resources :categories
-  get 'store' => 'store#index'
-  get 'blind' => 'store#blind'
-  get 'sight' => 'store#sight'
+  resources :reviews, except: [:show, :index]
+    collection { post :search, to: 'apps#index' }
+  end
 
-  match 'store/:id' => 'store#show', :as => :store_product, :via => :get
-end
+  #language
+  get 'set_language/english'
+  get 'set_language/german'
+
+  #get parameter, TODO: please remove them
+  get '/dictionary' => 'pages#dictionary'
+  get '/help' => 'help_items#index'
+  get '/disclaimer' => 'tabs#index'
+  get '/notification' => 'notifications#index'
+  get 'messages/new'
+  get 'pages/news'
 end
