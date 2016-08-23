8a33049bbf97bb9c356cc03daace45fcfdacfefe
Maria <zehuiwangzhw@gmail.com>
2016-08-23 11:30:51 +0200

    removed redundant view, cleaned routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,31 @@
 Rails.application.routes.draw do
+
+  #resources
   resources :help_items
   resources :tabs
-  get 'set_language/english'
+  resources :categories
+  resources :apps do
+    resources :reviews
+  end
 
+  #language
+  get 'set_language/english'
   get 'set_language/german'
 
+  #get parameter, TODO: please remove them
   get '/dictionary' => 'pages#dictionary'
   get '/help' => 'help_items#index'
   get '/disclaimer' => 'tabs#index'
   get '/notification' => 'notifications#index'
   get 'messages/new'
 
-Rails.application.routes.draw do
-  resources :help_items
-  resources :tabs
-  get 'set_language/english'
-
-  get 'set_language/german'
-
   scope "(:locale)", locale: /en|de/ do
     root to: "apps#index" 
     get "apps/index"
-  end  
+    match 'store/:id' => 'store#show', :as => :store_product, :via => :get
 end
 
-resources :apps do
-  resources :reviews
-end
+
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: { confirmations: 'confirmations' }
@@ -40,14 +39,6 @@ end
       get 'search'
     end
     resources :reviews, except: [:show, :index]
-    #collection { post :search, to: 'apps#index' }
-end
-  scope "(:locale)", locale: /en|de/ do
-  resources :categories
-  get 'store' => 'store#index'
-  get 'blind' => 'store#blind'
-  get 'sight' => 'store#sight'
-
-  match 'store/:id' => 'store#show', :as => :store_product, :via => :get
+    collection { post :search, to: 'apps#index' }
 end
 end
