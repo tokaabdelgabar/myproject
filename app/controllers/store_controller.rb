2476ac98eaf3b74c530c91b1256751f3995271ff
Maria <zehuiwangzhw@gmail.com>
2016-08-23 11:11:18 +0200

    done dynamically display categorized apps

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -9,12 +9,10 @@ class StoreController < ApplicationController
     @search = App.search(params[:q])
     @products = @search.result
     @search.build_condition if @search.conditions.empty?
-    @search.build_sort if @search.sorts.empty?
-    #@products = App.order(:price)     
+    @search.build_sort if @search.sorts.empty?    
   end
 
   def free
-
     @app=App.find(params[:category_id])
     @opp=App.where(price:'0') 
     @free=@app && @opp
@@ -24,29 +22,12 @@ class StoreController < ApplicationController
     @app=App.find(params[:category_id])
     @pai= App.where.not(price: '0') 
     @paid=@app && @paid
-
   end
 
   def show
     @product = Category.find(params[:id])
-    @search = App.ransack(params[:q]).find(params[:id])
-    @products = @search.result.joins(:reviews).select("apps.*, avg(reviews.rating) as average, count(*) as total").group("apps.id").where(Category.find(params[:id]))
+    @search = App.ransack(params[:q])
+    @products = @search.result.joins(:reviews).select("apps.*, avg(reviews.rating) as average, count(*) as total").group("apps.id").where(:category_id => params[:id])
     @search.build_sort if @search.sorts.empty?
-  end
-
-  def blind
-      #@name = params[:category_name]
-      #@dropdown = Language.ransack(params[:q])
-      @search = App.ransack(params[:q])
-      #@language = App.ransack(params[:q]).result.joins(:operatingsystems)
-      #@products = @search.result.where(:category_id => 1) 
-      @products = @search.result.joins(:reviews).select("apps.*, avg(reviews.rating) as average, count(*) as total").group("apps.id").where(:category_id => 1)
-      @search.build_sort if @search.sorts.empty?
-      @free = App.search(params[:q]).result.joins(:languages)
-  end
-
-  def sight
-      @search = App.search(params[:q])
-      @products = @search.result.where(:category_id => 2) 
   end
 end
