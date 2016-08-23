3344c03e5e81c61c4e1cdf0893020e37563c9ca9
Maria <zehuiwangzhw@gmail.com>
2016-08-23 11:57:40 +0200

    revert previous checkout

--- config/routes.rb
+++ config/routes.rb
@@ -1,43 +1,53 @@
 Rails.application.routes.draw do
-
-  #resources
   resources :help_items
   resources :tabs
-  resources :categories
-  resources :apps do
-    resources :reviews
-  end
-
-  #language
   get 'set_language/english'
+
   get 'set_language/german'
 
-  #get parameter, TODO: please remove them
   get '/dictionary' => 'pages#dictionary'
   get '/help' => 'help_items#index'
   get '/disclaimer' => 'tabs#index'
   get '/notification' => 'notifications#index'
   get 'messages/new'
-  get 'pages/news'
+
+Rails.application.routes.draw do
+  resources :help_items
+  resources :tabs
+  get 'set_language/english'
+
+  get 'set_language/german'
 
   scope "(:locale)", locale: /en|de/ do
     root to: "apps#index" 
     get "apps/index"
-    match 'store/:id' => 'store#show', :as => :store_product, :via => :get
-  end
+  end  
+end
+
+resources :apps do
+  resources :reviews
+end
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: { confirmations: 'confirmations' }
   resources :categories
 
-  #Contact
+# Contact
   resources :messages, only: [:new, :create]
 
-	resources :apps do
+  resources :apps do
     collection do
       get 'search'
     end
-  resources :reviews, except: [:show, :index]
-    collection { post :search, to: 'apps#index' }
-  end
+    resources :reviews, except: [:show, :index]
+    #collection { post :search, to: 'apps#index' }
+end
+  scope "(:locale)", locale: /en|de/ do
+  resources :categories
+  get 'store' => 'store#index'
+  get 'blind' => 'store#blind'
+  get 'sight' => 'store#sight'
+
+  match 'store/:id' => 'store#show', :as => :store_product, :via => :get
+end
 end
