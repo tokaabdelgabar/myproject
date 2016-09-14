da20a28dc873851025b481331cc84337eddac0f0
Toka <toka.abdelgabar@gmail.com>
2016-09-14 20:11:34 +0200

    reggistration extras

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,15 @@ class User < ActiveRecord::Base
 	# Include default devise modules. Others available are:
 	# :confirmable, :lockable, :timeoutable and :omniauthable
 	devise :database_authenticatable, :registerable, :confirmable, 
-	:recoverable, :rememberable, :trackable, :validatable
+	:recoverable, :rememberable, :trackable
 
-	validates_format_of :email,:with => Devise::email_regexp
+	validates_uniqueness_of  :email, :case_sensitive => false, :allow_blank => true, :if => :email_changed?
+    validates_format_of :email, :with  => Devise.email_regexp, :allow_blank => true, :if => :email_changed?
+    validates_presence_of :password, :on=>:create
+    validates_confirmation_of    :password, :on=>:create
+    validates :password_confirmation, :presence =>true
+    validates_length_of    :password, :within => Devise.password_length, :allow_blank => true
+ 
  #code for getting and setting the list of roles a user belongs to. 
  #This will perform the necessary bitwise operations to translate an array of roles into the integer field.
 
