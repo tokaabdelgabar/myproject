4618d7db75a1872ece57a300f97b41272155ac77
Mariatravelling <zehuiwangzhw@gmail.com>
2016-09-14 23:15:53 +0200

    translation

--- app/assets/javascripts/jquery.raty.js
+++ app/assets/javascripts/jquery.raty.js
@@ -730,7 +730,7 @@
     click        : undefined,
     half         : false,
     halfShow     : true,
-    hints        : ['bad', 'poor', 'regular', 'good', 'gorgeous'],
+    hints        : ['Mangelhaft', 'Ausreichend', 'Befriedigend', 'Gut', 'Sehr Gut'],
     iconRange    : undefined,
     mouseout     : undefined,
     mouseover    : undefined,
