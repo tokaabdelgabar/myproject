95231baf8f121efa7e062b30d29d01a54ec03b6d
Toka Abdelgabar <toka.abdelgabar@gmail.com>
2019-01-24 12:05:31 +0100

    updates

--- app/models/about_u.rb
+++ app/models/about_u.rb
@@ -1,4 +1,5 @@
 class AboutU < ActiveRecord::Base
-	has_attached_file :picture, :styles => { :app_index => "250x350>", :app_show => "325x475>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :picture, :styles => { :app_index => "250x350>", 
+		:app_show => "325x475>" }, :default_url => "/images/:style/missing.png"
     validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 end
