f42eda9305435d81d75511d1639844a7083424c2
Toka Abdelgabar <toka.abdelgabar@gmail.com>
2019-02-03 22:21:00 +0100

    update the app

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,18 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path.
+# Rails.application.config.assets.paths << Emoji.images_path
+# Add Yarn node_modules folder to the asset load path.
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+# Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.precompile += %w( bootstrap.min.css )
 Rails.application.config.assets.precompile += %w( category-apps.css )
 #Rails.application.config.assets.precompile += %w( custom.css )
@@ -29,9 +40,3 @@ Rails.application.config.assets.precompile += %w( style2.css )
 Rails.application.config.assets.precompile += %w( bootstrap-image-gallery.min.css )
 Rails.application.config.assets.precompile += %w( bootstrap-image-gallery.min.js )
 Rails.application.config.assets.precompile += %w( jquery-image-gallery.min.js )
-# Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Emoji.images_path
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
